8b4fc182746e79cdf6bc6cd989eb8b85d6db7b69
Ramiro Matteoda <ramiro.matteoda@3sharecorp.com>
2019-01-17 18:14:02 -0300

    update changes to heroku app

--- config/prod.exs
+++ config/prod.exs
@@ -11,14 +11,19 @@ use Mix.Config
 # before starting your production server.
 config :acv_demo, AcvDemoWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "localhost", port: 80],
+  url: [scheme: "https", host: "desolate-plateau-99973.herokuapp.com/", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  server: true,
-  root: ".",
-  version: Application.spec(:acv_demo, :vsn)
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure your database
+config :acv_demo, Hello.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
